f62f0ddfa0178889f80388c008b17289260cc129
ASHISHKHUNT <127084771+ASHISHKHUNT@users.noreply.github.com>
2023-03-06 14:35:47 +0530

    Update streamlit_app.py
    add fruit smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,10 @@ streamlit.header("Breakfast Favorites")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinatch & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free-Range Egg")
+
+
+streamlit.header("Build your own fruit smoothie")
+
+streamlit.text("Orange")
+streamlit.text("Apple Shake")
+streamlit.text("Banana Shake")
