81f481df6cfac0463411668c1161fabc77c49f5b
ASHISHKHUNT <127084771+ASHISHKHUNT@users.noreply.github.com>
2023-03-06 14:33:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My Mom's New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
+streamlit.header("Breakfast Favorites")
 
 streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinatch & Rocket Smoothie")
